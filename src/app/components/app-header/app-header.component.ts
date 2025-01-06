568ba67a826dcf1669fc0133c33db188efb46237
Lyubomir Vasilev <lyubomir.vasilev@adesso.bg>
2025-01-06 20:14:23 +0100

    add initial structure for site

---
+++ src/app/components/app-header/app-header.component.ts
@@ -0,0 +1,22 @@
+import {Component, HostListener} from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './app-header.component.html',
+  styleUrl: './app-header.component.css'
+})
+
+export class AppHeaderComponent {
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) {
+      header?.classList.add('shrink');
+    } else {
+      header?.classList.remove('shrink');
+    }
+  }
+}
