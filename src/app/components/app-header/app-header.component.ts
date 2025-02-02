9825990f78c60417f69bfdb5c394d44309164098
Lyubomir Vasilev <lyubomir.vasilev@adesso.bg>
2025-02-02 12:10:33 +0100

    add drop-down menu for translation

--- src/app/components/app-header/app-header.component.ts
+++ src/app/components/app-header/app-header.component.ts
@@ -8,13 +8,18 @@ import {TranslateService} from "@ngx-translate/core";
 })
 
 export class AppHeaderComponent {
+  selectedLanguage: string = 'main-en';
 
   constructor(private translate: TranslateService) {
     this.translate.setDefaultLang('main-en');
     this.translate.use('main-en');
   }
 
-  switchLanguage(language: string) {
-    this.translate.use(language);
+  public switchLanguage(event: any) {
+    const currentLanguage = event.target.value;
+    this.translate.use(currentLanguage);
+
+    //TODO Console line implemented only for test purposes, delete before merging into dev
+    console.log("Language changed to:", this.selectedLanguage);
   }
 }
