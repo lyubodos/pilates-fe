c1d243b837bac79fccaffb75523b2609971b5912
Lyubomir Vasilev <lyubomir.vasilev@adesso.bg>
2025-03-29 17:11:02 +0100

    add load spinner and add images for the gallery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,32 @@
-import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterOutlet} from '@angular/router';
 import {AppHeaderComponent} from "./components/app-header/app-header.component";
 import {AppFooterComponent} from "./components/app-footer/app-footer.component";
 import {AppModule} from "./app.module";
 import {TranslateService} from "@ngx-translate/core";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AppModule],
+  imports: [AppModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  isLoading = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.isLoading = true;
+      } else if (event instanceof NavigationEnd || event instanceof NavigationCancel || event instanceof NavigationError) {
+        this.isLoading = false;
+      }
+    });
+  }
+
+  public ngOnInit() {
+    console.log(this.isLoading)
+  }
 }
