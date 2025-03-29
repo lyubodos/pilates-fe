e5b722fa553029c86b1a28c33a0ea2b335f55584
Lyubomir Vasilev <lyubomir.vasilev@adesso.bg>
2025-03-29 16:55:43 +0100

    add carousel to the gallery section

--- src/app/components/app-main-section/components/gallery-page-section/gallery-page-section.component.ts
+++ src/app/components/app-main-section/components/gallery-page-section/gallery-page-section.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-gallery-page-section',
   templateUrl: './gallery-page-section.component.html',
   styleUrl: './gallery-page-section.component.scss'
 })
-export class GalleryPageSectionComponent {
+export class GalleryPageSectionComponent implements OnInit, OnDestroy {
+  public currentIndex = 0;
+  public interval: any;
+
+  public images = [
+    'assets/images/IMG_9881.jpeg',
+    'assets/images/IMG_9917.jpeg',
+    'assets/images/IMG_9881.jpeg',
+    'assets/images/IMG_9917.jpeg'
+  ];
+
+
+  public ngOnInit() {
+    this.interval = setInterval(() => {
+      this.nextSlide();
+    }, 3000);
+  }
+
+  public ngOnDestroy() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+
+  public nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  public prevSlide() {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
 
 }
