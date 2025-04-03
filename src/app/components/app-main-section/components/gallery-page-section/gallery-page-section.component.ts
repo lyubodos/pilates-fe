65c53730d3787ac5d39a6c7d165dadf540c06096
Lyubomir Vasilev <lyubomir.vasilev@adesso.bg>
2025-04-03 21:11:49 +0200

    add new feaures for home page header prices and gallery

--- src/app/components/app-main-section/components/gallery-page-section/gallery-page-section.component.ts
+++ src/app/components/app-main-section/components/gallery-page-section/gallery-page-section.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
+import {TimeInterval} from "rxjs/internal/operators/timeInterval";
 
 @Component({
   selector: 'app-gallery-page-section',
@@ -9,20 +10,28 @@ export class GalleryPageSectionComponent implements OnInit, OnDestroy {
   public currentIndex = 0;
   public interval: any;
 
+  private readonly INTERVAL = 4000;
+
   public images = [
-    'assets/images/home/IMG_0007.jpeg',
+    'assets/images/IMG_9979.jpeg',
+    'assets/images/IMG_9983.jpeg',
+    'assets/images/gallery/IMG_0313.jpeg',
     'assets/images/home/IMG_0024.jpeg',
     'assets/images/home/IMG_0025.jpeg',
     'assets/images/home/IMG_0030.jpeg',
-    'assets/images/IMG_9979.jpeg',
-    'assets/images/IMG_9983.jpeg'
+    'assets/images/gallery/IMG_0094.jpeg',
+    'assets/images/gallery/IMG_0115.jpeg',
+    'assets/images/gallery/IMG_0139.jpeg',
+    'assets/images/gallery/IMG_0192.jpeg',
+    'assets/images/gallery/IMG_0200.jpeg',
+    'assets/images/gallery/IMG_0239.jpeg',
   ];
 
 
   public ngOnInit() {
     this.interval = setInterval(() => {
       this.nextSlide();
-    }, 3000);
+    }, this.INTERVAL);
   }
 
   public ngOnDestroy() {
@@ -31,12 +40,27 @@ export class GalleryPageSectionComponent implements OnInit, OnDestroy {
     }
   }
 
+  public resetSlideInterval() {
+    clearInterval(this.interval);
+    this.interval = setInterval(() => {
+      this.nextSlide();
+    }, 5000);
+  }
+
   public nextSlide() {
     this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    this.resetSlideInterval();
   }
 
   public prevSlide() {
     this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+    this.resetSlideInterval();
+  }
+
+  private intervalGenerator() {
+    setInterval(() => {
+      this.nextSlide();
+    }, this.INTERVAL);
   }
 
 }
