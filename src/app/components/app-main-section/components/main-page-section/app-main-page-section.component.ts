0be4fe7438f7d990fbd7e55a22de5d0e0b0f464c
Lyubomir Vasilev <lyubomir.vasilev@adesso.bg>
2025-04-03 18:14:47 +0200

    adjust main colors for headers and header

--- src/app/components/app-main-section/components/main-page-section/app-main-page-section.component.ts
+++ src/app/components/app-main-section/components/main-page-section/app-main-page-section.component.ts
@@ -8,10 +8,30 @@ import {Router} from "@angular/router";
 })
 export class AppMainPageSectionComponent {
   private readonly contactsLink = "/contacts";
+  public faqs = [
+    {
+      question: 'What is the best time for training?',
+      answer: 'The best time depends on your schedule, but morning workouts boost metabolism, while evening sessions may improve performance.',
+      isOpen: false
+    },
+    {
+      question: 'How often should I work out?',
+      answer: 'For general fitness, aim for at least 3-5 sessions per week, balancing cardio and strength training.',
+      isOpen: false
+    },
+    {
+      question: 'Is weightlifting good for weight loss?',
+      answer: 'Yes! Strength training boosts metabolism, burns fat, and builds muscle, which helps with long-term weight management.',
+      isOpen: false
+    }
+  ];
 
   constructor(private router: Router) {
   }
 
+  public toggleFaq(index: number) {
+    this.faqs[index].isOpen = !this.faqs[index].isOpen;
+  }
 
   public navigateToContacts() {
 
