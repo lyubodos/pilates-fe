07514aeba4aadb13a833c24d24f4c0cf6c35bade
Lyubomir Vasilev <lyubomir.vasilev@adesso.bg>
2025-04-04 15:10:04 +0200

    add images rotation for main-hero

--- src/app/components/app-main-section/components/about-us-page-section/about-us-page-section.component.ts
+++ src/app/components/app-main-section/components/about-us-page-section/about-us-page-section.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+interface AboutUsCard {
+  title: string;
+  age: string;
+  image: string;
+  description: string;
+}
+
 @Component({
   selector: 'app-about-us-page-section',
   templateUrl: './about-us-page-section.component.html',
   styleUrl: './about-us-page-section.component.scss'
 })
 export class AboutUsPageSectionComponent {
+
+  public aboutUsCardsList: AboutUsCard[] = [
+    {
+      title: 'Veneta Vasileva',
+      age: '26',
+      image: 'assets/images/gallery/IMG_0239.jpeg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur dui interdum, tincidunt ipsum in, condimentum urna. Aenean ut cursus lacus, quis vestibulum diam. Nam consectetur sapien sit amet tristique semper. Praesent in malesuada quam, quis congue quam. Pellentesque at justo sed lacus vehicula rutrum eu sed libero. Duis at arcu orci. Nam efficitur mollis tempus.'
+    },
+    {
+      title: 'Geri Nikolova',
+      age: '24',
+      image: 'assets/images/about-us/IMG_0139_cropped.jpeg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur dui interdum, tincidunt ipsum in, condimentum urna. Aenean ut cursus lacus, quis vestibulum diam. Nam consectetur sapien sit amet tristique semper. Praesent in malesuada quam, quis congue quam. Pellentesque at justo sed lacus vehicula rutrum eu sed libero. Duis at arcu orci. Nam efficitur mollis tempus.'
+    }
+  ]
 
 }
--- src/app/components/app-main-section/components/main-page-section/app-main-page-section.component.ts
+++ src/app/components/app-main-section/components/main-page-section/app-main-page-section.component.ts
@@ -2,7 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {TranslateService} from "@ngx-translate/core";
 
-interface FaqStruct { question: string;
+interface FaqStruct {
+  question: string;
   answer: string;
   isOpen: boolean
 };
@@ -15,15 +16,28 @@ interface FaqStruct { question: string;
 })
 export class AppMainPageSectionComponent implements OnInit {
   private readonly contactsLink = "/contacts";
+  private readonly INTERVAL = 5000;
+
   public faqs: FaqStruct[] = [];
+  public currentIndex = 0;
+  public interval: any;
+
+
+  public imgs = [
+    'assets/images/IMG_9979.jpeg',
+    'assets/images/IMG_9983.jpeg',
+  ];
 
 
   constructor(private router: Router, private translateService: TranslateService) {
 
   }
 
-
   public ngOnInit() {
+    this.interval = setInterval(() => {
+      this.nextSlide();
+    }, this.INTERVAL);
+
     this.loadFaqs();
 
     this.translateService.onLangChange.subscribe(() => {
@@ -33,6 +47,10 @@ export class AppMainPageSectionComponent implements OnInit {
 
   public toggleFaq(index: number) {
     this.faqs[index].isOpen = !this.faqs[index].isOpen;
+  }
+
+  public nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.imgs.length;
   }
 
   public navigateToContacts() {
