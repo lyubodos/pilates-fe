8ca8f8eaff80147742e2c3622046965fec4d7aa7
Lyubomir Vasilev <lyubomir.vasilev@adesso.bg>
2025-05-06 13:52:20 +0200

    outsource testimonials generation in a service and adjust about-us veny img

--- src/app/components/app-main-section/components/contacts-page-section/contacts-page-section.component.ts
+++ src/app/components/app-main-section/components/contacts-page-section/contacts-page-section.component.ts
@@ -7,6 +7,7 @@ import {TestimonialsText} from "../../../shared/data/testimonials-text.data";
 import {TranslatedText} from "../../../shared/data/translated-text.data";
 import {TranslatingService} from "../../../../services/translating.service";
 import {Subscription} from "rxjs";
+import {TestimonialsService} from "../../../../services/testimonials.service";
 
 @Component({
   selector: 'app-contacts-page-section',
@@ -22,7 +23,7 @@ import {Subscription} from "rxjs";
     ])
   ]
 })
-export class ContactsPageSectionComponent implements OnInit{
+export class ContactsPageSectionComponent implements OnInit {
   public userForm: FormGroup = new FormGroup({});
   public isLoading: boolean = false;
   public showModal = false;
@@ -30,67 +31,10 @@ export class ContactsPageSectionComponent implements OnInit{
   public lang: keyof TranslatedText = 'en';
   private langSub!: Subscription;
 
-  public testimonials: TestimonialsText[]  = [
-    {
-      name: {
-        en: 'Minko Cvetanov',
-        bg: "Минко Цветанов"
-      },
-      image: 'assets/images/testimonials/Screenshot 2025-04-24 at 22.36.14.png',
-      info: {
-        en: "It is worth it! You won't regret it!",
-        bg: "Заслужава си! Няма да съжалявате!"
-      },
-      role: {
-        en: 'Body instructor/Gym trainer',
-        bg: "Фитнес инструктор"
-      },
-      message: {
-        en: "After visiting ReformFlow Studio, my focus shifted not only to fitness but also to the Pilates Reformer, which helps with perfect recovery and further improvement of physical form.",
-        bg: "След като посетих ReformFlow Studio моят поглед се фокусира не само върху фитнеса, но и пилатес реформъра, който спомага за перфектното възстановяване и доусъвършенстване на физическата форма."
-      }
-    },
-    {
-      name: {
-        en: 'Martina Karakoleva',
-        bg: "Мартина Караколева"
-      },
-      image: 'assets/images/testimonials/IMG_2189.jpeg',
-      info: {
-        en: "I can't wait to visit again!",
-        bg: "Нямам търпение за следващата ми тренировка!"
-      },
-      role: {
-        en: 'Marketing expert',
-        bg: "Маркетингов експерт"
-      },
-      message: {
-        en: "The instructors are exceptional professionals, working with great precision and paying attention to their clients with every single movement. I was very satisfied and can't wait for my next workout.",
-        bg: "Инструкторите са изключителни професионалисти, работещи с огромна прецизност и обръщат внимание на своите клиенти при всяко едно движение. Останах много доволна и нямам търпение за следващата ми тренировка. "
-      }
-    },
-    {
-      name: {
-        en: 'Daniela Mezeklieva',
-        bg: "Даниела Мезеклиева"
-      },
-      image: 'assets/images/testimonials/anonymous.jpeg',
-      info: {
-        en: 'These trainings improved my life!',
-        bg: "Тези тренировки подобриха живота ми!"
-      },
-      role: {
-        en: 'Receptionist',
-        bg: "Рецепционист"
-      },
-      message: {
-        en: "ReformFlow showed me exercising can be both intensive and fun. Professional team, driven to provide the best care for your body. I am always looking forward to my sessions.",
-        bg: "ReformFlow ми показа как упражненията могат да бъдат и натоварващи, и завабни. Професионален екип, който е посветен да предостави най-добрата гриьа за тялото ви. Винаги очаквам с нетърпение следващите сесии."
-      }
-    }
-  ];
+  public testimonials: TestimonialsText[] = []
 
-  constructor(private fb: UntypedFormBuilder, private reservationService: ReservationService, private translate: TranslateService, private translatingService: TranslatingService) {
+  constructor(private fb: UntypedFormBuilder, private reservationService: ReservationService, private translate: TranslateService, private translatingService: TranslatingService,
+              private testimonialsService: TestimonialsService) {
   }
 
   public ngOnInit() {
@@ -105,6 +49,8 @@ export class ContactsPageSectionComponent implements OnInit{
     this.langSub = this.translatingService.lang$.subscribe((lang) => {
       this.lang = lang;
     });
+
+    this.testimonials = this.testimonialsService.getAllTestimonials();
   }
 
   public getNzStatus(controlName: string): '' | 'error' | 'warning' {
--- src/app/components/app-main-section/components/main-page-section/app-main-page-section.component.ts
+++ src/app/components/app-main-section/components/main-page-section/app-main-page-section.component.ts
@@ -5,13 +5,13 @@ import {TranslatingService} from "../../../../services/translating.service";
 import {Subscription} from "rxjs";
 import {TestimonialsText} from "../../../shared/data/testimonials-text.data";
 import {TranslatedText} from "../../../shared/data/translated-text.data";
+import {TestimonialsService} from "../../../../services/testimonials.service";
 
 interface FaqStruct {
   question: string;
   answer: string;
   isOpen: boolean
 };
-
 
 
 @Component({
@@ -23,65 +23,7 @@ export class AppMainPageSectionComponent implements OnInit {
   public lang: keyof TranslatedText = 'en';
   private langSub!: Subscription;
 
-  public testimonials: TestimonialsText[]  = [
-    {
-      name: {
-        en: 'Minko Cvetanov',
-        bg: "Минко Цветанов"
-      },
-      image: 'assets/images/testimonials/Screenshot 2025-04-24 at 22.36.14.png',
-      info: {
-        en: "It is worth it! You won't regret it!",
-        bg: "Заслужава си! Няма да съжалявате!"
-      },
-      role: {
-        en: 'Body instructor/Gym trainer',
-        bg: "Фитнес инструктор"
-      },
-      message: {
-        en: "After visiting ReformFlow Studio, my focus shifted not only to fitness but also to the Pilates Reformer, which helps with perfect recovery and further improvement of physical form.",
-        bg: "След като посетих ReformFlow Studio моят поглед се фокусира не само върху фитнеса, но и пилатес реформъра, който спомага за перфектното възстановяване и доусъвършенстване на физическата форма."
-      }
-    },
-    {
-      name: {
-        en: 'Martina Karakoleva',
-        bg: "Мартина Караколева"
-      },
-      image: 'assets/images/testimonials/IMG_2189.jpeg',
-      info: {
-        en: "I can't wait to visit again!",
-        bg: "Нямам търпение за следващата ми тренировка!"
-      },
-      role: {
-        en: 'Marketing expert',
-        bg: "Маркетингов експерт"
-      },
-      message: {
-        en: "The instructors are exceptional professionals, working with great precision and paying attention to their clients with every single movement. I was very satisfied and can't wait for my next workout.",
-        bg: "Инструкторите са изключителни професионалисти, работещи с огромна прецизност и обръщат внимание на своите клиенти при всяко едно движение. Останах много доволна и нямам търпение за следващата ми тренировка. "
-      }
-    },
-    {
-      name: {
-        en: 'Daniela Mezeklieva',
-        bg: "Даниела Мезеклиева"
-      },
-      image: 'assets/images/testimonials/anonymous.jpeg',
-      info: {
-        en: 'These trainings improved my life!',
-        bg: "Тези тренировки подобриха живота ми!"
-      },
-      role: {
-        en: 'Receptionist',
-        bg: "Рецепционист"
-      },
-      message: {
-        en: "ReformFlow showed me exercising can be both intensive and fun. Professional team, driven to provide the best care for your body. I am always looking forward to my sessions.",
-        bg: "ReformFlow ми показа как упражненията могат да бъдат и натоварващи, и завабни. Професионален екип, който е посветен да предостави най-добрата гриьа за тялото ви. Винаги очаквам с нетърпение следващите сесии."
-      }
-    }
-  ];
+  public testimonials: TestimonialsText[] = [];
 
   private readonly contactsLink = "/contacts";
   private readonly INTERVAL = 5000;
@@ -98,7 +40,7 @@ export class AppMainPageSectionComponent implements OnInit {
   ];
 
 
-  constructor(private router: Router, private route: ActivatedRoute, private translateService: TranslateService, private translatingService: TranslatingService) {
+  constructor(private router: Router, private route: ActivatedRoute, private translateService: TranslateService, private translatingService: TranslatingService, private testimonialsService: TestimonialsService) {
 
   }
 
@@ -116,13 +58,15 @@ export class AppMainPageSectionComponent implements OnInit {
     this.translateService.onLangChange.subscribe(() => {
       this.loadFaqs();
     });
+
+    this.testimonials = this.testimonialsService.getAllTestimonials();
   }
 
   public ngAfterViewInit() {
     this.route.fragment.subscribe(fragment => {
       if (fragment) {
         setTimeout(() => {
-         this.scrollToWithOffset(fragment, 100);
+          this.scrollToWithOffset(fragment, 100);
         }, 0);
       }
     })
@@ -154,7 +98,7 @@ export class AppMainPageSectionComponent implements OnInit {
     const element = document.getElementById(id);
     if (element) {
       const y = element.getBoundingClientRect().top + window.pageYOffset - offset;
-      window.scrollTo({ top: y, behavior: 'smooth' });
+      window.scrollTo({top: y, behavior: 'smooth'});
     }
   }
 }
