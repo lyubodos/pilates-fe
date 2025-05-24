b2672cd74e395a09094f4db3bef977436bda24f6
Lyubomir Vasilev <lyubomir.vasilev@adesso.bg>
2025-05-24 17:27:19 +0200

    add load spiner and promise for aboul us card images as well

--- src/app/components/app-main-section/components/about-us-page-section/about-us-page-section.component.ts
+++ src/app/components/app-main-section/components/about-us-page-section/about-us-page-section.component.ts
@@ -16,6 +16,9 @@ interface AboutUsCard {
 export class AboutUsPageSectionComponent implements OnInit{
 
   public aboutUsCardsList: AboutUsCard[] = [];
+  public isLoading = false;
+  public imagesLoaded = false;
+
 
   constructor(private translateService: TranslateService) {
   }
@@ -32,5 +35,27 @@ export class AboutUsPageSectionComponent implements OnInit{
     this.translateService.get('MAIN.ABOUT_US.ABOUT_US_CARDS').subscribe((faqList: any[]) => {
       this.aboutUsCardsList = faqList;
     });
+
+    this.preloadImages().then(() => {
+      this.imagesLoaded = true;
+      this.isLoading = false;
+    }).catch(() => {
+      console.warn('Some images failed to preload.');
+      this.imagesLoaded = true;
+    });
+  }
+
+  private preloadImages() {
+    this.isLoading = true;
+    const preloadPromises = this.aboutUsCardsList.map((card) => {
+      return new Promise<void>((resolve, reject) => {
+        const img = new Image();
+        img.onload = () => resolve();
+        img.onerror = () => reject();
+        img.src = card.image;
+      });
+    });
+
+    return Promise.all(preloadPromises);
   }
 }
